b69c52301ff1034ad68230246f62dc3f2a272ba9
Sandeep Sarkar <152861071+sandy4242@users.noreply.github.com>
2025-09-25 23:58:19 +0530

    removed

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -4,8 +4,4 @@ class AppConstants {
   static const String chatLabel = "Chat";
   static const String profileLabel = "Profile";
   static const String circleOfInterestLabel = "CircleOfInterest";
-
-  // Add other navigation labels as needed
-  static const String createLabel = "Create";
-  static const String notificationsLabel = "Notifications";
 }
