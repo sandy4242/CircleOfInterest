11f7d460eb6d7904fed5b13f776a177f042f905b
Srijani Das <srijani0107@gmail.com>
2025-10-11 18:22:55 +0530

    Added splash_constants.dart

---
+++ lib/constants/splash_constants.dart
@@ -0,0 +1,13 @@
+class SplashConstants {
+  static const String logoAssetPath = 'assets/images/logo-no-bg.png';
+  static const String loadingText = 'Loading...';
+  static const String welcomeText = 'Welcome';
+  static const String loginRoute = '/login';
+  
+  static const int animationDurationMs = 2500;
+  static const int splashDelaySeconds = 5;
+  static const double logoSize = 120;
+  static const double logoPadding = 20;
+  static const double loadingIndicatorSize = 40;
+  static const double loadingIndicatorStrokeWidth = 2;
+}
